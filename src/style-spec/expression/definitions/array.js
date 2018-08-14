e7093acde0132d4ec1e07b2f51ee5776103988d9
John Firebaugh <john.firebaugh@gmail.com>
2018-08-13 17:29:05 -0700

    Extend array assertion to permit fallbacks

--- src/style-spec/expression/definitions/array.js
+++ src/style-spec/expression/definitions/array.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 const { toString, array, ValueType, StringType, NumberType, BooleanType, checkSubtype } = require('../types');
 
 const { typeOf } = require('../values');
@@ -10,58 +12,72 @@ const types = {
 };
 
 class ArrayAssertion {
-  constructor(type, input) {
+  constructor(type, args) {
     this.type = type;
-    this.input = input;
+    this.args = args;
   }
 
   static parse(args, context) {
-    if (args.length < 2 || args.length > 4)
-      return context.error(`Expected 1, 2, or 3 arguments, but found ${args.length - 1} instead.`);
+    if (args.length < 2) return context.error('Expected at least one argument.');
+
+    let i = 1;
 
     let itemType;
-    let N;
     if (args.length > 2) {
       const type = args[1];
       if (typeof type !== 'string' || !(type in types))
         return context.error('The item type argument of "array" must be one of string, number, boolean', 1);
       itemType = types[type];
+      i++;
     } else {
       itemType = ValueType;
     }
 
+    let N;
     if (args.length > 3) {
-      if (typeof args[2] !== 'number' || args[2] < 0 || args[2] !== Math.floor(args[2])) {
+      if (args[2] !== null && (typeof args[2] !== 'number' || args[2] < 0 || args[2] !== Math.floor(args[2]))) {
         return context.error('The length argument to "array" must be a positive integer literal', 2);
       }
       N = args[2];
+      i++;
     }
 
     const type = array(itemType, N);
 
-    const input = context.parse(args[args.length - 1], args.length - 1, ValueType);
-    if (!input) return null;
+    const parsed = [];
+    for (; i < args.length; i++) {
+      const input = context.parse(args[i], i, ValueType);
+      if (!input) return null;
+      parsed.push(input);
+    }
 
-    return new ArrayAssertion(type, input);
+    return new ArrayAssertion(type, parsed);
   }
 
   evaluate(ctx) {
-    const value = this.input.evaluate(ctx);
-    const error = checkSubtype(this.type, typeOf(value));
-    if (error) {
-      throw new RuntimeError(
-        `Expected value to be of type ${toString(this.type)}, but found ${toString(typeOf(value))} instead.`
-      );
+    for (let i = 0; i < this.args.length; i++) {
+      const value = this.args[i].evaluate(ctx);
+      const error = checkSubtype(this.type, typeOf(value));
+      if (!error) {
+        return value;
+      }
+      if (i === this.args.length - 1) {
+        throw new RuntimeError(
+          `Expected value to be of type ${toString(this.type)}, but found ${toString(typeOf(value))} instead.`
+        );
+      }
     }
-    return value;
+
+    assert(false);
+    return null;
   }
 
   eachChild(fn) {
-    fn(this.input);
+    this.args.forEach(fn);
   }
 
   possibleOutputs() {
-    return this.input.possibleOutputs();
+    return [].concat(...this.args.map(arg => arg.possibleOutputs()));
   }
 
   serialize() {
@@ -70,12 +86,11 @@ class ArrayAssertion {
     if (itemType.kind === 'string' || itemType.kind === 'number' || itemType.kind === 'boolean') {
       serialized.push(itemType.kind);
       const N = this.type.N;
-      if (typeof N === 'number') {
+      if (typeof N === 'number' || this.args.length > 1) {
         serialized.push(N);
       }
     }
-    serialized.push(this.input.serialize());
-    return serialized;
+    return serialized.concat(this.args.map(arg => arg.serialize()));
   }
 }
 
