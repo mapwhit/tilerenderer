8d1a54e5442b45310246325df7a5e5f2ac4b41cd
Damian Krzeminski <pirxpilot@furkot.com>
2025-03-07 16:11:40 -0700

    use static fields for ColorMode

--- src/gl/color_mode.js
+++ src/gl/color_mode.js
@@ -5,17 +5,17 @@ const ONE = 0x0001;
 const ONE_MINUS_SRC_ALPHA = 0x0303;
 
 class ColorMode {
+  static Replace = [ONE, ZERO];
+
+  static disabled = new ColorMode(ColorMode.Replace, Color.transparent, [false, false, false, false]);
+  static unblended = new ColorMode(ColorMode.Replace, Color.transparent, [true, true, true, true]);
+  static alphaBlended = new ColorMode([ONE, ONE_MINUS_SRC_ALPHA], Color.transparent, [true, true, true, true]);
+
   constructor(blendFunction, blendColor, mask) {
     this.blendFunction = blendFunction;
     this.blendColor = blendColor;
     this.mask = mask;
   }
 }
-
-ColorMode.Replace = [ONE, ZERO];
-
-ColorMode.disabled = new ColorMode(ColorMode.Replace, Color.transparent, [false, false, false, false]);
-ColorMode.unblended = new ColorMode(ColorMode.Replace, Color.transparent, [true, true, true, true]);
-ColorMode.alphaBlended = new ColorMode([ONE, ONE_MINUS_SRC_ALPHA], Color.transparent, [true, true, true, true]);
 
 module.exports = ColorMode;
