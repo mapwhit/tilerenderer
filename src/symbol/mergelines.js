2f98d20ead35f47eadc2a6d3b2c462ab7ea3f6f7
Damian Krzeminski <pirxpilot@furkot.com>
2025-03-08 09:00:22 -0700

    rewrite mergelines

--- src/symbol/mergelines.js
+++ src/symbol/mergelines.js
@@ -1,75 +1,75 @@
 module.exports = function (features) {
-  const leftIndex = {};
-  const rightIndex = {};
+  const leftIndex = new Map();
+  const rightIndex = new Map();
   const mergedFeatures = [];
   let mergedIndex = 0;
 
-  function add(k) {
-    mergedFeatures.push(features[k]);
-    mergedIndex++;
-  }
-
-  function mergeFromRight(leftKey, rightKey, geom) {
-    const i = rightIndex[leftKey];
-    delete rightIndex[leftKey];
-    rightIndex[rightKey] = i;
-
-    mergedFeatures[i].geometry[0].pop();
-    mergedFeatures[i].geometry[0] = mergedFeatures[i].geometry[0].concat(geom[0]);
-    return i;
-  }
-
-  function mergeFromLeft(leftKey, rightKey, geom) {
-    const i = leftIndex[rightKey];
-    delete leftIndex[rightKey];
-    leftIndex[leftKey] = i;
-
-    mergedFeatures[i].geometry[0].shift();
-    mergedFeatures[i].geometry[0] = geom[0].concat(mergedFeatures[i].geometry[0]);
-    return i;
-  }
-
-  function getKey(text, geom, onRight) {
-    const point = onRight ? geom[0][geom[0].length - 1] : geom[0][0];
-    return `${text}:${point.x}:${point.y}`;
-  }
-
   for (let k = 0; k < features.length; k++) {
-    const feature = features[k];
-    const geom = feature.geometry;
-    const text = feature.text;
+    const { geometry, text } = features[k];
 
     if (!text) {
       add(k);
       continue;
     }
 
-    const leftKey = getKey(text, geom);
-    const rightKey = getKey(text, geom, true);
+    const leftKey = getKey(text, geometry);
+    const rightKey = getKey(text, geometry, true);
 
-    if (leftKey in rightIndex && rightKey in leftIndex && rightIndex[leftKey] !== leftIndex[rightKey]) {
+    if (rightIndex.has(leftKey) && leftIndex.has(rightKey) && rightIndex.get(leftKey) !== leftIndex.get(rightKey)) {
       // found lines with the same text adjacent to both ends of the current line, merge all three
-      const j = mergeFromLeft(leftKey, rightKey, geom);
+      const j = mergeFromLeft(leftKey, rightKey, geometry);
       const i = mergeFromRight(leftKey, rightKey, mergedFeatures[j].geometry);
 
-      delete leftIndex[leftKey];
-      delete rightIndex[rightKey];
+      leftIndex.delete(leftKey);
+      rightIndex.delete(rightKey);
 
-      rightIndex[getKey(text, mergedFeatures[i].geometry, true)] = i;
+      rightIndex.set(getKey(text, mergedFeatures[i].geometry, true), i);
       mergedFeatures[j].geometry = null;
-    } else if (leftKey in rightIndex) {
+    } else if (rightIndex.has(leftKey)) {
       // found mergeable line adjacent to the start of the current line, merge
-      mergeFromRight(leftKey, rightKey, geom);
-    } else if (rightKey in leftIndex) {
+      mergeFromRight(leftKey, rightKey, geometry);
+    } else if (leftIndex.has(rightKey)) {
       // found mergeable line adjacent to the end of the current line, merge
-      mergeFromLeft(leftKey, rightKey, geom);
+      mergeFromLeft(leftKey, rightKey, geometry);
     } else {
       // no adjacent lines, add as a new item
       add(k);
-      leftIndex[leftKey] = mergedIndex - 1;
-      rightIndex[rightKey] = mergedIndex - 1;
+      leftIndex.set(leftKey, mergedIndex - 1);
+      rightIndex.set(rightKey, mergedIndex - 1);
     }
   }
 
   return mergedFeatures.filter(f => f.geometry);
+
+  function add(k) {
+    mergedFeatures.push(features[k]);
+    mergedIndex++;
+  }
+
+  function mergeFromRight(leftKey, rightKey, [geom]) {
+    const i = rightIndex.get(leftKey);
+    rightIndex.delete(leftKey);
+    rightIndex.set(rightKey, i);
+
+    const feature = mergedFeatures[i];
+    feature.geometry[0].pop();
+    feature.geometry[0].push(...geom);
+    return i;
+  }
+
+  function mergeFromLeft(leftKey, rightKey, [geom]) {
+    const i = leftIndex.get(rightKey);
+    leftIndex.delete(rightKey);
+    leftIndex.set(leftKey, i);
+
+    const feature = mergedFeatures[i];
+    feature.geometry[0].shift();
+    feature.geometry[0].unshift(...geom);
+    return i;
+  }
+
+  function getKey(text, [geom], onRight) {
+    const { x, y } = geom.at(onRight ? -1 : 0);
+    return `${text}:${x}:${y}`;
+  }
 };
