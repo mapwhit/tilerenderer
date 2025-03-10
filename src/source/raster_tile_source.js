cdd1d2acb0c6c3e24648666322a88cf2dec7b4f2
Damian Krzeminski <pirxpilot@furkot.com>
2025-03-10 16:53:56 -0600

    remove callback from `loadTileJSON`
    please note - we are calling it with await to delay sending events

--- src/source/raster_tile_source.js
+++ src/source/raster_tile_source.js
@@ -24,22 +24,21 @@ class RasterTileSource extends Evented {
     Object.assign(this, pick(options, ['url', 'scheme', 'tileSize']));
   }
 
-  load() {
+  async load() {
     this.fire(new Event('dataloading', { dataType: 'source' }));
-    loadTileJSON(this._options, (err, tileJSON) => {
-      if (err) {
-        this.fire(new ErrorEvent(err));
-      } else if (tileJSON) {
-        Object.assign(this, tileJSON);
-        if (tileJSON.bounds) this.tileBounds = new TileBounds(tileJSON.bounds, this.minzoom, this.maxzoom);
+    try {
+      const tileJSON = await loadTileJSON(this._options);
+      Object.assign(this, tileJSON);
+      if (tileJSON.bounds) this.tileBounds = new TileBounds(tileJSON.bounds, this.minzoom, this.maxzoom);
 
-        // `content` is included here to prevent a race condition where `Style#_updateSources` is called
-        // before the TileJSON arrives. this makes sure the tiles needed are loaded once TileJSON arrives
-        // ref: https://github.com/mapbox/mapbox-gl-js/pull/4347#discussion_r104418088
-        this.fire(new Event('data', { dataType: 'source', sourceDataType: 'metadata' }));
-        this.fire(new Event('data', { dataType: 'source', sourceDataType: 'content' }));
-      }
-    });
+      // `content` is included here to prevent a race condition where `Style#_updateSources` is called
+      // before the TileJSON arrives. this makes sure the tiles needed are loaded once TileJSON arrives
+      // ref: https://github.com/mapbox/mapbox-gl-js/pull/4347#discussion_r104418088
+      this.fire(new Event('data', { dataType: 'source', sourceDataType: 'metadata' }));
+      this.fire(new Event('data', { dataType: 'source', sourceDataType: 'content' }));
+    } catch (err) {
+      this.fire(new ErrorEvent(err));
+    }
   }
 
   onAdd(map) {
