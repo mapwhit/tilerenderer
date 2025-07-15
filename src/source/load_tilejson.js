b8eaba47f1c8cafbc89ed7b5b9dbc686b4e18ef4
Damian Krzeminski <pirxpilot@furkot.com>
2025-07-15 09:26:00 -0600

    remove unused support for `resourceSets` in map style

--- src/source/load_tilejson.js
+++ src/source/load_tilejson.js
@@ -1,8 +1,6 @@
 const { pick } = require('../util/object');
 
-module.exports = function (tileJSON) {
-  return tileJSON.resourceSets ? fromResourseSets(tileJSON) : fromTileJSON(tileJSON);
-};
+module.exports = fromTileJSON;
 
 function fromTileJSON(tileJSON) {
   const result = pick(tileJSON, ['tiles', 'minzoom', 'maxzoom', 'attribution', 'bounds']);
@@ -10,24 +8,5 @@ function fromTileJSON(tileJSON) {
     result.vectorLayers = tileJSON.vector_layers;
     result.vectorLayerIds = result.vectorLayers.map(layer => layer.id);
   }
-  return result;
-}
-
-function fromResourseSets(tileJSON) {
-  const { resourceSets } = tileJSON;
-  if (!resourceSets.length) {
-    throw new Error('expected resources');
-  }
-  const { resources } = resourceSets[0];
-  if (!resources?.length) {
-    throw new Error('expected resources');
-  }
-  const { imageUrl, imageUrlSubdomains } = resources[0];
-  const result = {
-    tiles: imageUrlSubdomains.map(sub => imageUrl.replace('{subdomain}', sub).replace('http:', 'https:')),
-    ...tileJSON
-  };
-  // expects already loaded object, `url` property is ignored
-  delete result.url;
   return result;
 }
