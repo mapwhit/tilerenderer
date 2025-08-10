342cff051322f39ff369d1ac5847539103344374
Damian Krzeminski <pirxpilot@furkot.com>
2025-08-09 20:58:42 -0600

    clean-up worker sources

--- src/source/geojson_worker_source.js
+++ src/source/geojson_worker_source.js
@@ -5,64 +5,11 @@ const { default: Supercluster } = require('supercluster');
 const { default: geojsonvt } = require('geojson-vt');
 const VectorTileWorkerSource = require('./vector_tile_worker_source');
 
-function loadGeoJSONTile(params) {
-  if (!this._geoJSONIndex) {
-    if (!this._createGeoJSONIndex) {
-      return; // we couldn't load the file
-    }
-
-    try {
-      this._geoJSONIndex = this._createGeoJSONIndex();
-    } finally {
-      this._createGeoJSONIndex = null;
-    }
-  }
-
-  const { z, x, y } = params.tileID.canonical;
-  const geoJSONTile = this._geoJSONIndex.getTile(z, x, y);
-  if (!geoJSONTile) {
-    return; // nothing in the given tile
-  }
-
-  const geojsonWrapper = new GeoJSONWrapper(geoJSONTile.features);
-
-  // Encode the geojson-vt tile into binary vector tile form.  This
-  // is a convenience that allows `FeatureIndex` to operate the same way
-  // across `VectorTileSource` and `GeoJSONSource` data.
-  let pbf = fromVectorTileJs(geojsonWrapper);
-  if (pbf.byteOffset !== 0 || pbf.byteLength !== pbf.buffer.byteLength) {
-    // Compatibility with node Buffer (https://github.com/mapbox/pbf/issues/35)
-    pbf = new Uint8Array(pbf);
-  }
-
-  return {
-    vectorTile: geojsonWrapper,
-    rawData: pbf.buffer
-  };
-}
-
 /**
  * The {@link WorkerSource} implementation that supports {@link GeoJSONSource}.
- * This class is designed to be easily reused to support custom source types
- * for data formats that can be parsed/converted into an in-memory GeoJSON
- * representation.  To do so, create it with
- * `new GeoJSONWorkerSource(resources, layerIndex, customLoadGeoJSONFunction)`.
- * For a full example, see [mapbox-gl-topojson](https://github.com/developmentseed/mapbox-gl-topojson).
  *
  */
 class GeoJSONWorkerSource extends VectorTileWorkerSource {
-  /**
-   * @param [loadGeoJSON] Optional method for custom loading/parsing of
-   * GeoJSON based on parameters passed from the main-thread Source.
-   * See {@link GeoJSONWorkerSource#loadGeoJSON}.
-   */
-  constructor(resources, layerIndex, loadGeoJSON) {
-    super(resources, layerIndex, loadGeoJSONTile);
-    if (loadGeoJSON) {
-      this.loadGeoJSON = loadGeoJSON;
-    }
-  }
-
   /**
    * Fetches (if appropriate), parses, and index geojson data into tiles. This
    * preparatory method must be called before {@link GeoJSONWorkerSource#loadTile}
@@ -76,7 +23,7 @@ class GeoJSONWorkerSource extends VectorTileWorkerSource {
    * @param callback
    */
   loadData(params) {
-    const data = this.loadGeoJSON(params);
+    const data = loadJSON(params.data);
     this._geoJSONIndex = null;
     this._createGeoJSONIndex = params.cluster
       ? () => {
@@ -89,20 +36,56 @@ class GeoJSONWorkerSource extends VectorTileWorkerSource {
         };
   }
 
-  /**
-   * Fetch and parse GeoJSON according to the given params.
-   *
-   * GeoJSON is expected as a literal (string or object) `params.data`.
-   *
-   * @param params
-   * @param [params.data] Literal GeoJSON data. Must be provided.
-   */
-  loadGeoJSON(params) {
-    try {
-      return JSON.parse(params.data);
-    } catch (e) {
-      throw new Error('Input data is not a valid GeoJSON object.');
+  getTile(tileID) {
+    if (!this._geoJSONIndex) {
+      if (!this._createGeoJSONIndex) {
+        return; // we couldn't load the file
+      }
+
+      try {
+        this._geoJSONIndex = this._createGeoJSONIndex();
+      } finally {
+        this._createGeoJSONIndex = null;
+      }
     }
+    const { z, x, y } = tileID.canonical;
+    return this._geoJSONIndex.getTile(z, x, y);
+  }
+
+  loadVectorData({ tileID }) {
+    const geoJSONTile = this.getTile(tileID);
+    if (!geoJSONTile) {
+      return; // nothing in the given tile
+    }
+
+    const vectorTile = new GeoJSONWrapper(geoJSONTile.features);
+
+    // Encode the geojson-vt tile into binary vector tile form.  This
+    // is a convenience that allows `FeatureIndex` to operate the same way
+    // across `VectorTileSource` and `GeoJSONSource` data.
+    let pbf = fromVectorTileJs(vectorTile);
+    if (pbf.byteOffset !== 0 || pbf.byteLength !== pbf.buffer.byteLength) {
+      // Compatibility with node Buffer (https://github.com/mapbox/pbf/issues/35)
+      pbf = new Uint8Array(pbf);
+    }
+
+    return {
+      vectorTile,
+      rawData: pbf.buffer
+    };
+  }
+}
+
+/**
+ * Fetch and parse GeoJSON according to the given params.
+ *
+ * @param data Literal GeoJSON data. Must be provided.
+ */
+function loadJSON(data) {
+  try {
+    return JSON.parse(data);
+  } catch (e) {
+    throw new Error('Input data is not a valid GeoJSON object.');
   }
 }
 
