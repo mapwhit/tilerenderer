c1a7f7004de9a1adca7b49d90ce95a413681611d
Damian Krzeminski <pirxpilot@furkot.com>
2025-08-26 14:56:07 -0600

    use `@map/geometry` instead of utils
    finding pole of inaccessibility, intersection tests and some {x,y}
    geometry utils are now moved to an external module

--- src/util/util.js
+++ src/util/util.js
@@ -105,61 +105,6 @@ export function getCoordinatesCenter(coords) {
 }
 
 /**
- * Indicates if the provided Points are in a counter clockwise (true) or clockwise (false) order
- *
- * @private
- * @returns true for a counter clockwise set of points
- */
-// http://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
-export function isCounterClockwise(a, b, c) {
-  return (c.y - a.y) * (b.x - a.x) > (b.y - a.y) * (c.x - a.x);
-}
-
-/**
- * Returns the signed area for the polygon ring.  Postive areas are exterior rings and
- * have a clockwise winding.  Negative areas are interior rings and have a counter clockwise
- * ordering.
- *
- * @private
- * @param ring Exterior or interior ring
- */
-export function calculateSignedArea(ring) {
-  let sum = 0;
-  const len = ring.length;
-  let p2 = ring[len - 1]; // last point
-  for (const p1 of ring) {
-    sum += (p2.x - p1.x) * (p1.y + p2.y);
-    p2 = p1;
-  }
-  return sum;
-}
-
-/**
- * Detects closed polygons, first + last point are equal
- *
- * @private
- * @param points array of points
- * @return true if the points are a closed polygon
- */
-export function isClosedPolygon(points) {
-  // If it is 2 points that are the same then it is a point
-  // If it is 3 points with start and end the same then it is a line
-  if (points.length < 4) {
-    return false;
-  }
-
-  const p1 = points[0];
-  const p2 = points[points.length - 1];
-
-  if (Math.abs(p1.x - p2.x) > 0 || Math.abs(p1.y - p2.y) > 0) {
-    return false;
-  }
-
-  // polygon simplification can produce polygons with zero area and more than 3 points
-  return Math.abs(calculateSignedArea(points)) > 0.01;
-}
-
-/**
  * Converts spherical coordinates to cartesian coordinates.
  *
  * @private
