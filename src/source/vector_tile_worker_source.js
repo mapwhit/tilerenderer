250ab1a9f0917fbc952e878338ab91363af0f171
Natalia Kowalczyk <melitele@code42day.com>
2025-08-27 02:19:48 -0600

    simplify VectorTileWorkerSource as it doesn't need to be reused
    VectorTileWorkerSource does so little that it makes more sense
    to create other custom sources independently than reusing it

--- src/source/vector_tile_worker_source.js
+++ src/source/vector_tile_worker_source.js
@@ -4,17 +4,11 @@ import makeWorkerTile from './worker_tile.js';
 
 /**
  * The {@link WorkerSource} implementation that supports {@link VectorTileSource}.
- * This class is designed to be easily reused to support custom source types
- * for data formats that can be parsed/converted into an in-memory VectorTile
- * representation.  To do so, create it with
- * `new VectorTileWorkerSource(resources, styleLayers, customLoadVectorDataFunction)`.
  *
  * @private
  */
 class VectorTileWorkerSource {
   /**
-   * @param [loadVectorData] Optional method for custom loading of a VectorTile
-   * object based on parameters passed from the main-thread Source. See
    * {@link VectorTileWorkerSource#loadTile}. The default implementation simply
    * loads the pbf at `params.url`.
    */
@@ -23,7 +17,10 @@ class VectorTileWorkerSource {
     this.layerIndex = layerIndex;
   }
 
-  loadVectorData(params) {
+  /**
+   * Implements {@link WorkerSource#loadTile}.
+   */
+  async loadTile(params) {
     if (!params.response) {
       throw new Error('no tile data');
     }
@@ -31,26 +28,9 @@ class VectorTileWorkerSource {
     if (!data) {
       return;
     }
-    return {
-      vectorTile: new VectorTile(new Protobuf(data))
-    };
-  }
-
-  /**
-   * Implements {@link WorkerSource#loadTile}. Delegates to
-   * {@link VectorTileWorkerSource#loadVectorData} (which by default expects
-   * a `params.url` property) for fetching and producing a VectorTile object.
-   */
-  async loadTile(params) {
-    const response = this.loadVectorData(params);
-    if (!response) {
-      return;
-    }
-    const { vectorTile } = response;
+    const vectorTile = new VectorTile(new Protobuf(data));
     const result = await makeWorkerTile(params, vectorTile, this.layerIndex, this.resources);
-    if (vectorTile) {
-      result.vectorTile = vectorTile;
-    }
+    result.vectorTile = vectorTile;
     return result;
   }
 }
