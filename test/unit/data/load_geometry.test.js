0b188f80c44f2f444d865448e45ceccdd103e587
Natalia Kowalczyk <melitele@code42day.com>
2025-08-28 02:02:34 -0600

    refactor loading sample vector tile in all tests to use common function

--- test/unit/data/load_geometry.test.js
+++ test/unit/data/load_geometry.test.js
@@ -1,32 +1,32 @@
-import fs from 'node:fs';
-import path from 'node:path';
 import test from 'node:test';
-import Protobuf from '@mapwhit/pbf';
-import { VectorTile } from '@mapwhit/vector-tile';
 import loadGeometry from '../../../src/data/load_geometry.js';
+import { loadVectorTile } from '../../util/tile.js';
 
-// Load a line feature from fixture tile.
-const vt = new VectorTile(
-  new Protobuf(fs.readFileSync(path.join(import.meta.dirname, '/../../fixtures/mbsv5-6-18-23.vector.pbf')))
-);
+test('loadGeometry', async t => {
+  let sourceLayer;
+  t.before(() => {
+    // Load line features from fixture tile.
+    sourceLayer = loadVectorTile().layers.road;
+  });
 
-test('loadGeometry', t => {
-  const feature = vt.layers.road.feature(0);
-  const originalGeometry = feature.loadGeometry();
-  const scaledGeometry = loadGeometry(feature);
-  t.assert.equal(scaledGeometry[0][0].x, originalGeometry[0][0].x * 2, 'scales x coords by 2x');
-  t.assert.equal(scaledGeometry[0][0].y, originalGeometry[0][0].y * 2, 'scales y coords by 2x');
-});
+  await t.test('loadGeometry', t => {
+    const feature = sourceLayer.feature(0);
+    const originalGeometry = feature.loadGeometry();
+    const scaledGeometry = loadGeometry(feature);
+    t.assert.equal(scaledGeometry[0][0].x, originalGeometry[0][0].x * 2, 'scales x coords by 2x');
+    t.assert.equal(scaledGeometry[0][0].y, originalGeometry[0][0].y * 2, 'scales y coords by 2x');
+  });
 
-test('loadGeometry extent error', t => {
-  const feature = vt.layers.road.feature(0);
-  feature.extent = 1024;
+  await t.test('loadGeometry extent error', t => {
+    const feature = sourceLayer.feature(0);
+    feature.extent = 1024;
 
-  const warn = t.mock.method(console, 'warn', () => {});
-  loadGeometry(feature);
-  t.assert.equal(warn.mock.callCount(), 1);
-  t.assert.match(
-    warn.mock.calls[0].arguments[0],
-    /Geometry exceeds allowed extent, reduce your vector tile buffer size/
-  );
+    const warn = t.mock.method(console, 'warn', () => {});
+    loadGeometry(feature);
+    t.assert.equal(warn.mock.callCount(), 1);
+    t.assert.match(
+      warn.mock.calls[0].arguments[0],
+      /Geometry exceeds allowed extent, reduce your vector tile buffer size/
+    );
+  });
 });
--- test/unit/util/classify_rings.test.js
+++ test/unit/util/classify_rings.test.js
@@ -1,84 +1,33 @@
-import fs from 'node:fs';
-import path from 'node:path';
 import test from 'node:test';
-import Protobuf from '@mapwhit/pbf';
-import { VectorTile } from '@mapwhit/vector-tile';
 import classifyRings from '../../../src/util/classify_rings.js';
+import { loadVectorTile } from '../../util/tile.js';
 
-// Load a fill feature from fixture tile.
-const vt = new VectorTile(
-  new Protobuf(fs.readFileSync(path.join(import.meta.dirname, '/../../fixtures/mbsv5-6-18-23.vector.pbf')))
-);
-const feature = vt.layers.water.feature(0);
+test('classifyRings', async t => {
+  let feature;
+  t.before(() => {
+    // Load fill features from fixture tile.
+    const sourceLayer = loadVectorTile().layers.water;
+    feature = sourceLayer.feature(0);
+  });
 
-test('classifyRings', t => {
-  let geometry;
-  let classified;
+  await t.test('classifyRings', t => {
+    let geometry;
+    let classified;
 
-  geometry = [
-    [
-      { x: 0, y: 0 },
-      { x: 0, y: 40 },
-      { x: 40, y: 40 },
-      { x: 40, y: 0 },
-      { x: 0, y: 0 }
-    ]
-  ];
-  classified = classifyRings(geometry);
-  t.assert.equal(classified.length, 1, '1 polygon');
-  t.assert.equal(classified[0].length, 1, 'polygon 1 has 1 exterior');
+    geometry = [
+      [
+        { x: 0, y: 0 },
+        { x: 0, y: 40 },
+        { x: 40, y: 40 },
+        { x: 40, y: 0 },
+        { x: 0, y: 0 }
+      ]
+    ];
+    classified = classifyRings(geometry);
+    t.assert.equal(classified.length, 1, '1 polygon');
+    t.assert.equal(classified[0].length, 1, 'polygon 1 has 1 exterior');
 
-  geometry = [
-    [
-      { x: 0, y: 0 },
-      { x: 0, y: 40 },
-      { x: 40, y: 40 },
-      { x: 40, y: 0 },
-      { x: 0, y: 0 }
-    ],
-    [
-      { x: 60, y: 0 },
-      { x: 60, y: 40 },
-      { x: 100, y: 40 },
-      { x: 100, y: 0 },
-      { x: 60, y: 0 }
-    ]
-  ];
-  classified = classifyRings(geometry);
-  t.assert.equal(classified.length, 2, '2 polygons');
-  t.assert.equal(classified[0].length, 1, 'polygon 1 has 1 exterior');
-  t.assert.equal(classified[1].length, 1, 'polygon 2 has 1 exterior');
-
-  geometry = [
-    [
-      { x: 0, y: 0 },
-      { x: 0, y: 40 },
-      { x: 40, y: 40 },
-      { x: 40, y: 0 },
-      { x: 0, y: 0 }
-    ],
-    [
-      { x: 10, y: 10 },
-      { x: 20, y: 10 },
-      { x: 20, y: 20 },
-      { x: 10, y: 10 }
-    ]
-  ];
-  classified = classifyRings(geometry);
-  t.assert.equal(classified.length, 1, '1 polygon');
-  t.assert.equal(classified[0].length, 2, 'polygon 1 has 1 exterior, 1 interior');
-
-  geometry = feature.loadGeometry();
-  classified = classifyRings(geometry);
-  t.assert.equal(classified.length, 2, '2 polygons');
-  t.assert.equal(classified[0].length, 1, 'polygon 1 has 1 exterior');
-  t.assert.equal(classified[1].length, 10, 'polygon 2 has 1 exterior, 9 interior');
-});
-
-test('classifyRings + maxRings', async t => {
-  function createGeometry(options) {
-    const geometry = [
-      // Outer ring, area = 3200
+    geometry = [
       [
         { x: 0, y: 0 },
         { x: 0, y: 40 },
@@ -86,14 +35,27 @@ test('classifyRings + maxRings', async t => {
         { x: 40, y: 0 },
         { x: 0, y: 0 }
       ],
-      // Inner ring, area = 100
       [
-        { x: 30, y: 30 },
-        { x: 32, y: 30 },
-        { x: 32, y: 32 },
-        { x: 30, y: 30 }
+        { x: 60, y: 0 },
+        { x: 60, y: 40 },
+        { x: 100, y: 40 },
+        { x: 100, y: 0 },
+        { x: 60, y: 0 }
+      ]
+    ];
+    classified = classifyRings(geometry);
+    t.assert.equal(classified.length, 2, '2 polygons');
+    t.assert.equal(classified[0].length, 1, 'polygon 1 has 1 exterior');
+    t.assert.equal(classified[1].length, 1, 'polygon 2 has 1 exterior');
+
+    geometry = [
+      [
+        { x: 0, y: 0 },
+        { x: 0, y: 40 },
+        { x: 40, y: 40 },
+        { x: 40, y: 0 },
+        { x: 0, y: 0 }
       ],
-      // Inner ring, area = 4
       [
         { x: 10, y: 10 },
         { x: 20, y: 10 },
@@ -101,49 +63,87 @@ test('classifyRings + maxRings', async t => {
         { x: 10, y: 10 }
       ]
     ];
-    if (options?.reverse) {
-      geometry[0].reverse();
-      geometry[1].reverse();
-      geometry[2].reverse();
+    classified = classifyRings(geometry);
+    t.assert.equal(classified.length, 1, '1 polygon');
+    t.assert.equal(classified[0].length, 2, 'polygon 1 has 1 exterior, 1 interior');
+
+    geometry = feature.loadGeometry();
+    classified = classifyRings(geometry);
+    t.assert.equal(classified.length, 2, '2 polygons');
+    t.assert.equal(classified[0].length, 1, 'polygon 1 has 1 exterior');
+    t.assert.equal(classified[1].length, 10, 'polygon 2 has 1 exterior, 9 interior');
+  });
+
+  await t.test('classifyRings + maxRings', async t => {
+    function createGeometry(options) {
+      const geometry = [
+        // Outer ring, area = 3200
+        [
+          { x: 0, y: 0 },
+          { x: 0, y: 40 },
+          { x: 40, y: 40 },
+          { x: 40, y: 0 },
+          { x: 0, y: 0 }
+        ],
+        // Inner ring, area = 100
+        [
+          { x: 30, y: 30 },
+          { x: 32, y: 30 },
+          { x: 32, y: 32 },
+          { x: 30, y: 30 }
+        ],
+        // Inner ring, area = 4
+        [
+          { x: 10, y: 10 },
+          { x: 20, y: 10 },
+          { x: 20, y: 20 },
+          { x: 10, y: 10 }
+        ]
+      ];
+      if (options?.reverse) {
+        geometry[0].reverse();
+        geometry[1].reverse();
+        geometry[2].reverse();
+      }
+      return geometry;
     }
-    return geometry;
-  }
 
-  await t.test('maxRings=undefined', t => {
-    const geometry = sortRings(classifyRings(createGeometry()));
-    t.assert.equal(geometry.length, 1);
-    t.assert.equal(geometry[0].length, 3);
-    t.assert.equal(geometry[0][0].area, 3200);
-    t.assert.equal(geometry[0][1].area, 100);
-    t.assert.equal(geometry[0][2].area, 4);
-  });
-
-  await t.test('maxRings=2', t => {
-    const geometry = sortRings(classifyRings(createGeometry(), 2));
-    t.assert.equal(geometry.length, 1);
-    t.assert.equal(geometry[0].length, 2);
-    t.assert.equal(geometry[0][0].area, 3200);
-    t.assert.equal(geometry[0][1].area, 100);
-  });
-
-  await t.test('maxRings=2, reversed geometry', t => {
-    const geometry = sortRings(classifyRings(createGeometry({ reverse: true }), 2));
-    t.assert.equal(geometry.length, 1);
-    t.assert.equal(geometry[0].length, 2);
-    t.assert.equal(geometry[0][0].area, 3200);
-    t.assert.equal(geometry[0][1].area, 100);
-  });
-
-  await t.test('maxRings=5, geometry from fixture', t => {
-    const geometry = sortRings(classifyRings(feature.loadGeometry(), 5));
-    t.assert.equal(geometry.length, 2);
-    t.assert.equal(geometry[0].length, 1);
-    t.assert.equal(geometry[1].length, 5);
-
-    const areas = geometry[1].map(ring => {
-      return ring.area;
+    await t.test('maxRings=undefined', t => {
+      const geometry = sortRings(classifyRings(createGeometry()));
+      t.assert.equal(geometry.length, 1);
+      t.assert.equal(geometry[0].length, 3);
+      t.assert.equal(geometry[0][0].area, 3200);
+      t.assert.equal(geometry[0][1].area, 100);
+      t.assert.equal(geometry[0][2].area, 4);
     });
-    t.assert.deepEqual(areas, [2763951, 21600, 8298, 4758, 3411]);
+
+    await t.test('maxRings=2', t => {
+      const geometry = sortRings(classifyRings(createGeometry(), 2));
+      t.assert.equal(geometry.length, 1);
+      t.assert.equal(geometry[0].length, 2);
+      t.assert.equal(geometry[0][0].area, 3200);
+      t.assert.equal(geometry[0][1].area, 100);
+    });
+
+    await t.test('maxRings=2, reversed geometry', t => {
+      const geometry = sortRings(classifyRings(createGeometry({ reverse: true }), 2));
+      t.assert.equal(geometry.length, 1);
+      t.assert.equal(geometry[0].length, 2);
+      t.assert.equal(geometry[0][0].area, 3200);
+      t.assert.equal(geometry[0][1].area, 100);
+    });
+
+    await t.test('maxRings=5, geometry from fixture', t => {
+      const geometry = sortRings(classifyRings(feature.loadGeometry(), 5));
+      t.assert.equal(geometry.length, 2);
+      t.assert.equal(geometry[0].length, 1);
+      t.assert.equal(geometry[1].length, 5);
+
+      const areas = geometry[1].map(ring => {
+        return ring.area;
+      });
+      t.assert.deepEqual(areas, [2763951, 21600, 8298, 4758, 3411]);
+    });
   });
 });
 
