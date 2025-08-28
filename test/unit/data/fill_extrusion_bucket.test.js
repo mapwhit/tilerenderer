0cf1726ec12367d031e86218f5163bc779ffadfd
Natalia Kowalczyk <melitele@code42day.com>
2025-08-27 21:56:49 -0600

    refactor bucket tests to use common test utilities

--- test/unit/data/fill_extrusion_bucket.test.js
+++ test/unit/data/fill_extrusion_bucket.test.js
@@ -1,38 +1,40 @@
-import fs from 'node:fs';
-import path from 'node:path';
 import test from 'node:test';
-import Protobuf from '@mapwhit/pbf';
-import { VectorTile } from '@mapwhit/vector-tile';
 import FillExtrusionBucket from '../../../src/data/bucket/fill_extrusion_bucket.js';
-import FeatureIndex from '../../../src/data/feature_index.js';
 import { FillExtrusionStyleLayer } from '../../../src/style/style_layer/fill_extrusion_style_layer.js';
+import { createPopulateOptions, getFeaturesFromLayer, loadVectorTile } from '../../util/tile.js';
 
-// Load a fill feature from fixture tile.
-const vt = new VectorTile(
-  new Protobuf(fs.readFileSync(path.join(import.meta.dirname, '/../../fixtures/mbsv5-6-18-23.vector.pbf')))
-);
-
-test('FillExtrusionBucket fill-pattern with global-state', t => {
-  const globalState = { pattern: 'test-pattern' };
+function createFillExtrusionBucket({ id, layout, paint, globalState }) {
   const layer = new FillExtrusionStyleLayer({
-    id: 'test',
+    id,
     type: 'fill-extrusion',
-    paint: { 'fill-extrusion-pattern': ['coalesce', ['get', 'pattern'], ['global-state', 'pattern']] }
+    layout,
+    paint
   });
-  layer.recalculate({ zoom: 0, globalState });
+  layer.recalculate({ zoom: 0, zoomHistory: {}, globalState });
 
-  const bucket = new FillExtrusionBucket({ layers: [layer], globalState });
+  return new FillExtrusionBucket({ layers: [layer], globalState });
+}
 
-  const sourceLayer = vt.layers.water;
-  const features = new Array(sourceLayer.length);
-  for (let i = 0; i < sourceLayer.length; i++) {
-    features[i] = { feature: sourceLayer.feature(i) };
-  }
+test('FillExtrusionBucket', async t => {
+  let sourceLayer;
+  t.before(() => {
+    // Load fill extrusion features from fixture tile.
+    sourceLayer = loadVectorTile().layers.water;
+  });
 
-  bucket.populate(features, { patternDependencies: {}, featureIndex: new FeatureIndex() });
+  await t.test('FillExtrusionBucket fill-pattern with global-state', t => {
+    const bucket = createFillExtrusionBucket({
+      id: 'test',
+      layout: {},
+      paint: { 'fill-extrusion-pattern': ['coalesce', ['get', 'pattern'], ['global-state', 'pattern']] },
+      globalState: { pattern: 'test-pattern' }
+    });
 
-  t.assert.ok(bucket.features.length > 0);
-  t.assert.deepEqual(bucket.features[0].patterns, {
-    test: { min: 'test-pattern', mid: 'test-pattern', max: 'test-pattern' }
+    bucket.populate(getFeaturesFromLayer(sourceLayer), createPopulateOptions());
+
+    t.assert.ok(bucket.features.length > 0);
+    t.assert.deepEqual(bucket.features[0].patterns, {
+      test: { min: 'test-pattern', mid: 'test-pattern', max: 'test-pattern' }
+    });
   });
 });
