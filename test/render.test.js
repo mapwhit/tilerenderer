7fd846578c0679d458b3536adb7179ccb519edd6
Damian Krzeminski <pirxpilot@furkot.com>
2025-09-01 12:48:09 -0600

    remove test/integration/index.js

--- test/render.test.js
+++ test/render.test.js
@@ -1,5 +1,5 @@
 import ignores from './ignores.json' with { type: 'json' };
-import { render } from './integration/index.js';
+import render from './integration/lib/render.js';
 import suiteImplementation from './suite_implementation.js';
 import getArgs from './util/args.js';
 import _window from './util/window.js';
