3641d8b4a061948cd8d573d2d0903b022d5d48b9
Damian Krzeminski <pirxpilot@furkot.com>
2025-09-03 21:19:52 -0600

    remove args

--- test/query.test.js
+++ test/query.test.js
@@ -3,14 +3,12 @@ import ignores from './ignores.json' with { type: 'json' };
 import harness from './integration/lib/harness.js';
 import queryTest from './integration/lib/query.js';
 import suiteImplementation from './suite_implementation.js';
-import getArgs from './util/args.js';
 import _window from './util/window.js';
 
 globalThis.window ??= _window;
 
 const options = {
-  ignores,
-  ...getArgs()
+  ignores
 };
 
 const implementation = 'js';
