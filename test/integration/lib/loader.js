0d9934eed9cb0b872cd283019694633e0d0ab9db
Damian Krzeminski <pirxpilot@furkot.com>
2025-09-05 07:42:52 -0600

    fix async loader

--- test/integration/lib/loader.js
+++ test/integration/lib/loader.js
@@ -3,15 +3,15 @@ import { readFile } from 'node:fs/promises';
 import path from 'node:path';
 import colors from 'chalk';
 
-export default function () {
-  // /test/integration
-  const integrationPath = path.join(import.meta.dirname, '..');
-  // mvt-fixtures -> /test/integration/node_modules/@mapbox/mvt-fixtures
-  const mapboxMVTFixturesPath = path.join(
-    path.dirname(new URL(import.meta.resolve('@mapbox/mvt-fixtures')).pathname),
-    '..'
-  );
+// /test/integration
+const integrationPath = path.join(import.meta.dirname, '..');
+// mvt-fixtures -> /test/integration/node_modules/@mapbox/mvt-fixtures
+const mapboxMVTFixturesPath = path.join(
+  path.dirname(new URL(import.meta.resolve('@mapbox/mvt-fixtures')).pathname),
+  '..'
+);
 
+export default function () {
   function localizeURL(url) {
     return url.replace(/^local:\/\//, '');
   }
@@ -150,26 +150,28 @@ export default function () {
     localizeURLs: async function (style) {
       await localizeStyleURLs(style);
       if (style.metadata?.test?.operations) {
-        style.metadata.test.operations.forEach(op => {
-          if (op[0] === 'addSource') {
-            localizeSourceURLs(op[2]);
-          } else if (op[0] === 'setStyle') {
-            if (typeof op[1] === 'object') {
-              localizeStyleURLs(op[1]);
-              return;
+        await Promise.all(
+          style.metadata.test.operations.map(async op => {
+            if (op[0] === 'addSource') {
+              await localizeSourceURLs(op[2]);
+            } else if (op[0] === 'setStyle') {
+              if (typeof op[1] === 'object') {
+                await localizeStyleURLs(op[1]);
+                return;
+              }
+
+              const styleJSON = loadJSON(op[1]);
+              if (!styleJSON) {
+                return;
+              }
+
+              await localizeStyleURLs(styleJSON);
+
+              op[1] = styleJSON;
+              op[2] = { diff: false };
             }
-
-            const styleJSON = loadJSON(op[1]);
-            if (!styleJSON) {
-              return;
-            }
-
-            localizeStyleURLs(styleJSON);
-
-            op[1] = styleJSON;
-            op[2] = { diff: false };
-          }
-        });
+          })
+        );
       }
     },
     loadJSON
