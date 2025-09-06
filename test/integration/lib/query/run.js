3c1793f7c09881ecfe8d004f720cc54a50f1b6f7
Damian Krzeminski <pirxpilot@furkot.com>
2025-09-06 09:08:46 -0600

    tell harness to optionally generate prefix for test name
    this ensures that we can see full test name when running test from
    `integration/(render|query)/tests/` subdirectory

--- test/integration/lib/query/run.js
+++ test/integration/lib/query/run.js
@@ -2,10 +2,8 @@ import harness from '../harness.js';
 import suiteImplementation from '../suite_implementation.js';
 import validate from './validate.js';
 
-const implementation = 'js';
-
-export default function run(directory) {
-  harness(directory, implementation, runTest);
+export default function run(directory, { prefix, implementation = 'js' } = {}) {
+  harness(directory, runTest, { implementation, prefix });
 
   async function runTest(style, params) {
     const { data, results } = await suiteImplementation(style, params);
