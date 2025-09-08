cb969b325fa8626ec868fae4bee8dc2f256d1430
melitele <melitele@furkot.com>
2025-09-08 02:16:46 -0600

    refactor `CircleStyleLayer.queryIntersectsFeature` for reuse

--- src/style/query_utils.js
+++ src/style/query_utils.js
@@ -1,4 +1,8 @@
+import glMatrix from '@mapbox/gl-matrix';
+import { polygonIntersectsBufferedPoint } from '@mapwhit/geometry';
 import { Point } from '@mapwhit/point-geometry';
+
+const { vec4 } = glMatrix;
 
 export function getMaximumPaintValue(property, layer, bucket) {
   const value = layer._paint.get(property).value;
@@ -27,4 +31,78 @@ export function translate(queryGeometry, translate, translateAnchor, bearing, pi
   pt;
 
   return queryGeometry.map(point => point.sub(pt));
+}
+
+const circleIntersectionTests = new Map([
+  [
+    'map',
+    new Map([
+      [
+        'map',
+        ({ queryGeometry, point, size }) => {
+          return polygonIntersectsBufferedPoint(queryGeometry, point, size);
+        }
+      ],
+      [
+        'viewport',
+        ({ queryGeometry, point, pixelPosMatrix, size, transform }) => {
+          const projectedCenter = vec4.transformMat4([], [point.x, point.y, 0, 1], pixelPosMatrix);
+          const adjustedSize = (size * projectedCenter[3]) / transform.cameraToCenterDistance;
+          return polygonIntersectsBufferedPoint(queryGeometry, point, adjustedSize);
+        }
+      ]
+    ])
+  ],
+  [
+    'viewport',
+    new Map([
+      [
+        'map',
+        ({ queryGeometry, point, pixelPosMatrix, size, transform }) => {
+          const projectedCenter = vec4.transformMat4([], [point.x, point.y, 0, 1], pixelPosMatrix);
+          const adjustedSize = (size * transform.cameraToCenterDistance) / projectedCenter[3];
+          return polygonIntersectsBufferedPoint(queryGeometry, projectPoint(point, pixelPosMatrix), adjustedSize);
+        }
+      ],
+      [
+        'viewport',
+        ({ queryGeometry, point, pixelPosMatrix, size }) => {
+          return polygonIntersectsBufferedPoint(queryGeometry, projectPoint(point, pixelPosMatrix), size);
+        }
+      ]
+    ])
+  ]
+]);
+
+export function circleIntersection({
+  queryGeometry,
+  geometry,
+  pixelPosMatrix,
+  size,
+  transform,
+  pitchAlignment = 'map',
+  pitchScale = 'map'
+}) {
+  const intersectionTest = circleIntersectionTests.get(pitchAlignment).get(pitchScale);
+  const param = { queryGeometry, pixelPosMatrix, size, transform };
+  for (const ring of geometry) {
+    for (const point of ring) {
+      param.point = point;
+      if (intersectionTest(param)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function projectPoint(p, pixelPosMatrix) {
+  const point = vec4.transformMat4([], [p.x, p.y, 0, 1], pixelPosMatrix);
+  return new Point(point[0] / point[3], point[1] / point[3]);
+}
+
+export function projectQueryGeometry(queryGeometry, pixelPosMatrix) {
+  return queryGeometry.map(p => {
+    return projectPoint(p, pixelPosMatrix);
+  });
 }
